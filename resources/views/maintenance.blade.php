dabc921cbeb8ba7f75242734e82d7dc4c65abece
BenjaCod <bociulisbenjaminas@gmail.com>
2024-10-31 15:33:02 +0200

    Maintenance mode

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <script src="https://kit.fontawesome.com/8bdc128fe8.js" crossorigin="anonymous"></script>
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body class="bg-gray-100">
+    <div class="flex justify-center">
+        <div class="text-center p-10">
+            <H1 class="text-5xl font-bold mt-10">Šiuo metu svetainė kuriama</H1>
+            <p class="text-3xl font-bold mt-5">Prašome pabandyti vėliau</p>
+        </div>
+    </div>
+@livewireScripts
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Userdatabaserender;
 
-Route::view('/', 'home');
+// Route::view('/', 'home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
     
-Route::get('/c/{cname}', userdatabaserender::class);
+// Route::get('/c/{cname}', userdatabaserender::class);
+
+// Maintenance mode:
+
+Route::view('/', 'maintenance');
 
 require __DIR__.'/auth.php';
